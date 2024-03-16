a1f48b3dd4b41bb90beee169f84973da58516724
Awais <4w415@live.co.uk>
2024-03-16 15:59:30 +0000

    Added rounding to the output.

--- Test_Submission/calculator.py
+++ Test_Submission/calculator.py
@@ -1,9 +1,16 @@
+print("""
+      This program allows you to divide two numbers, please enter 0 as the second number to exit the loop.
+      """)
 while True:
 
     Value1 = float(input("Please enter number 1: "))
     Value2 = float(input("Please enter number 2: "))
 
+    if Value2 == 0:
+        print("Divisor can't be 0, program closing.")
+        break
+
     def division(a,b):
-        return a/b
+        return f"{a/b:.2f}"
 
     print(division(Value1,Value2))
