852c9944880a99cdd1910319695aea8cf4129ed3
Awais <4w415@live.co.uk>
2024-03-16 15:36:01 +0000

    Version 1 complete

---
+++ Test_Submission/calculator.py
@@ -0,0 +1,9 @@
+while True:
+
+    Value1 = float(input("Please enter number 1: "))
+    Value2 = float(input("Please enter number 2: "))
+
+    def division(a,b):
+        return a/b
+
+    print(division(Value1,Value2))
