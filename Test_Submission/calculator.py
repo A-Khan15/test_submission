84eee7ca23e58b189adc29260e0e8da0b2178011
Awais <4w415@live.co.uk>
2024-03-16 15:56:50 +0000

    Added a way to exit loop and no more division by 0.

--- Test_Submission/calculator.py
+++ Test_Submission/calculator.py
@@ -1,7 +1,14 @@
+print("""
+      This program allows you to divide two numbers, please enter 0 as the second number to exit the loop.
+      """)
 while True:
 
     Value1 = float(input("Please enter number 1: "))
     Value2 = float(input("Please enter number 2: "))
+
+    if Value2 == 0:
+        print("Divisor can't be 0, program closing.")
+        break
 
     def division(a,b):
         return a/b
